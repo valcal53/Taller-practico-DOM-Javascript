8e0f0dba0958f47da5ed09a7f04ae0ae179c173e
Santiago Valencia Calderón <santaiagovalenciacalderon55@gmail.com>
2025-03-08 15:37:11 -0500

    Cambio dinámico de contenido

--- Ejercicio 1/app.js
+++ Ejercicio 1/app.js
@@ -1,8 +1,7 @@
 const boton = document.getElementById('Boton');
         const parrafo = document.getElementById('Parrafo');
         
-        // Asignar evento al hacer clic en el botón
+        
         boton.addEventListener('click', function() {
-            // Cambiar el contenido del parrafo
             parrafo.textContent = '¡Texto cambiado!';
         });
