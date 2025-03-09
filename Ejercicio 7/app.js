f18a6f6ee88b33b7c45fe1fd976b54cc3ad1b76b
Santiago Valencia Calderón <santaiagovalenciacalderon55@gmail.com>
2025-03-09 18:03:09 -0500

    Validación de formulario

--- Ejercicio 7/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let passwordInput = document.getElementById("password");
-    let togglePassword = document.getElementById("togglePassword");
-
-    togglePassword.addEventListener("change", function() {
-       
-        passwordInput.type = this.checked ? "text" : "password";
-    });
-});
