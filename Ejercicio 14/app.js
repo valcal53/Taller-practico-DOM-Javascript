3390e096f8922056c782024f444c511696c937e7
Santiago Valencia Calderón <santaiagovalenciacalderon55@gmail.com>
2025-03-10 23:16:20 -0500

    Notificación emergente personalizada

---
+++ Ejercicio 14/app.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    
+    const notificacion = document.getElementById('notificacion');
+
+    
+    setTimeout(function() {
+        notificacion.classList.add('oculta'); 
+    }, 3000); 
+};
